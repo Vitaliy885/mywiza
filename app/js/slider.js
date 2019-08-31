47729a69d26ff9de6467d4db7db87588d2ce6ea3
vitalii <vitalik@gmail.com>
2019-08-31 22:09:51 +0300

    fix

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,6 +1,16 @@
-new Siema({
+var mySiema = new Siema({
  selector: '.our__reviews-list',
  duration: 2000,
  perPage: 3,
- loop: true 
+ loop: true,
+ perPage: {
+    768: 2,
+    1024: 3,
+  },
 });
+
+var prev = document.querySelector('.left__arrow');
+var next = document.querySelector('.right__arrow');
+
+prev.addEventListener('click', () => mySiema.prev());
+next.addEventListener('click', () => mySiema.next());
