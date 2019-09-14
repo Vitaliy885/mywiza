319486765ceadf150fc887839137d3f9671ac617
Vitaliy885 <Vitalik885@gmail.com>
2019-09-15 00:29:17 +0300

    fix

---
+++ dist/js/index.js
@@ -0,0 +1,11 @@
+(function(){
+
+	var btnMenu = document.querySelector('.btn__menu');
+	var menu = document.querySelector('.navbar');
+
+	btnMenu.addEventListener('click', function(){
+		menu.classList.toggle('active-bar');
+		btnMenu.classList.toggle('active-burger');
+	});
+
+})();
