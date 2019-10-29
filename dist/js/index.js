d9e71fe77eafdf2387072f641737100703c1d233
Vitalik885 <Vitalik885@gmail.com>
2019-10-29 23:50:23 +0200

    fix

--- dist/js/index.js
+++ dist/js/index.js
@@ -8,3 +8,17 @@
 		btnMenu.classList.toggle('active-burger');
 	});
 })();
+
+
+window.onscroll = function() {
+	if(window.pageYOffset > 1){
+document.querySelector('.top__line-header').style.display = 'none';
+document.querySelector('.navbar__line').classList.add('scrollingHeader');
+document.querySelector('.scrollingBlockHidden').style.height = 69.2 +'px';
+	}else{
+		document.querySelector('.top__line-header').style.display = 'block';
+	document.querySelector('.navbar__line').classList.remove('scrollingHeader');
+	document.querySelector('.scrollingBlockHidden').style.height = 0 +'px';
+	}
+}
+
